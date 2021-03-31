c5bc7d239534100fb7ef598932f48782a975169c
franciscoguinarte <francisco.guinarte@outlook.com>
2021-03-30 23:53:10 -0300

    Project hr-payroll created

---
+++ hr-payroll/src/test/java/com/devsuperior/payroll/HrPayrollApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.payroll;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrPayrollApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
