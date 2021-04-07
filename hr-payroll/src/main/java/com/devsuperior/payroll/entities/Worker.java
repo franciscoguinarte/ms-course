b1598c87410f337ac2d9dabed0f491a41424dbda
franciscoguinarte <francisco.guinarte@outlook.com>
2021-04-07 13:22:03 -0300

    Add rest template for payroll service

---
+++ hr-payroll/src/main/java/com/devsuperior/payroll/entities/Worker.java
@@ -0,0 +1,76 @@
+package com.devsuperior.payroll.entities;
+
+import java.io.Serializable;
+
+
+public class Worker implements Serializable {
+	 
+	private static final long serialVersionUID = 1L;
+	
+	private Long id;
+	private String name;
+	
+	public Worker() {
+		super();
+	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		long temp;
+		temp = Double.doubleToLongBits(dailyIncome);
+		result = prime * result + (int) (temp ^ (temp >>> 32));
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Worker other = (Worker) obj;
+		if (Double.doubleToLongBits(dailyIncome) != Double.doubleToLongBits(other.dailyIncome))
+			return false;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		return true;
+	}
+	public Worker(Long id, String name, double dailyIncome) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.dailyIncome = dailyIncome;
+	}
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public double getDailyIncome() {
+		return dailyIncome;
+	}
+	public void setDailyIncome(double dailyIncome) {
+		this.dailyIncome = dailyIncome;
+	}
+	private double dailyIncome;
+
+}
