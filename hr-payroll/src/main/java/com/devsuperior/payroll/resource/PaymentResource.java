28e11f5e52853466529bc0aebdf43aaeb8c35b44
franciscoguinarte <francisco.guinarte@outlook.com>
2021-04-12 15:52:52 -0300

    git add hysytix tolerance

--- hr-payroll/src/main/java/com/devsuperior/payroll/resource/PaymentResource.java
+++ hr-payroll/src/main/java/com/devsuperior/payroll/resource/PaymentResource.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.devsuperior.payroll.entities.Payment;
 import com.devsuperior.payroll.services.PaymentService;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 
 @RestController
 @RequestMapping("/payments")
@@ -17,10 +18,18 @@ public class PaymentResource {
 	@Autowired
 	private PaymentService paymentService; 
 	
+	@HystrixCommand(fallbackMethod = "getPaymentAlternative")
 	@GetMapping("/{workerId}/days/{days}")
 	public ResponseEntity<Payment> getPayment(@PathVariable Long workerId, @PathVariable int days){
 		
 		Payment payment = paymentService.getPayment(workerId, days);
+		
+		return ResponseEntity.ok(payment);
+	}
+	
+	public ResponseEntity<Payment> getPaymentAlternative(Long workerId, int days){
+	
+		Payment payment = new Payment("Francisco", 400, days);
 		
 		return ResponseEntity.ok(payment);
 	}
