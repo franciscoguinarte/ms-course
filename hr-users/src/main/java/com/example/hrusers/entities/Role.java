d3b1012503cd8d7159af11c9d0191c737c548dc5
franciscoguinarte <francisco.guinarte@outlook.com.br>
2021-04-18 22:32:14 -0300

    add many to many on entities: user->role

---
+++ hr-users/src/main/java/com/example/hrusers/entities/Role.java
@@ -0,0 +1,46 @@
+package com.example.hrusers.entities;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Table(name = "tb_role")
+@Entity
+public class Role implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String roleName;
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getRoleName() {
+		return roleName;
+	}
+	public void setRoleName(String roleName) {
+		this.roleName = roleName;
+	}
+	
+	public Role(Long id, String roleName) {
+		super();
+		this.id = id;
+		this.roleName = roleName;
+	}
+
+	public Role() {
+		
+	}
+}
