3f72b494e3ac5ce4a265de7860fedcc38c6c9ee0
franciscoguinarte <francisco.guinarte@outlook.com.br>
2021-04-19 21:49:09 -0300

    add seed to hr-users database

--- hr-users/src/main/java/com/example/hrusers/HrUsersApplication.java
+++ hr-users/src/main/java/com/example/hrusers/HrUsersApplication.java
@@ -1,15 +1,27 @@
 package com.example.hrusers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @EnableEurekaClient
 @SpringBootApplication
-public class HrUsersApplication {
+public class HrUsersApplication implements CommandLineRunner {
 
+	@Autowired
+	private BCryptPasswordEncoder passwordEncoder;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(HrUsersApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		// TODO Auto-generated method stub
+		System.out.println("BCRIPT -- " + passwordEncoder.encode("123456") );
 	}
 
 }
