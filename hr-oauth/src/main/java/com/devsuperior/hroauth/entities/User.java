f0d54d9d06781988551279c9d97d963035d673d9
franciscoguinarte <francisco.guinarte@outlook.com.br>
2021-04-26 09:04:06 -0300

    Login with Jwt Token

--- hr-oauth/src/main/java/com/devsuperior/hroauth/entities/User.java
+++ hr-oauth/src/main/java/com/devsuperior/hroauth/entities/User.java
@@ -1,10 +1,16 @@
 package com.devsuperior.hroauth.entities;
 
 import java.io.Serializable;
+import java.util.Collection;
 import java.util.HashSet;
 import java.util.Set;
+import java.util.stream.Collectors;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
  
-public class User implements Serializable {
+public class User implements UserDetails, Serializable {
  
 	private static final long serialVersionUID = 1L;
 	
@@ -60,5 +66,38 @@ public class User implements Serializable {
  
 	
 	private Set<Role> roles = new HashSet<>();
+	
+	
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		// TODO Auto-generated method stub
+		return roles.stream().map(x -> new SimpleGrantedAuthority(x.getRoleName())).
+				collect(Collectors.toList());
+	}
+	@Override
+	public String getUsername() {
+		 
+		return email;
+	}
+	@Override
+	public boolean isAccountNonExpired() {
+		// TODO Auto-generated method stub
+		return true;
+	}
+	@Override
+	public boolean isAccountNonLocked() {
+		// TODO Auto-generated method stub
+		return true;
+	}
+	@Override
+	public boolean isCredentialsNonExpired() {
+		// TODO Auto-generated method stub
+		return true;
+	}
+	@Override
+	public boolean isEnabled() {
+		// TODO Auto-generated method stub
+		return true;
+	}
 	
 }
